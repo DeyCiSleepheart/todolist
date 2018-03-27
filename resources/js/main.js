910802d74bf749977f305f65717513216a6891c0
Morten Dalsgaard <mortendalsgaard42@gmail.com>
2018-03-27 22:00:55 +0200

    adding the dlt btn + cmplt btn functionality

--- resources/js/main.js
+++ resources/js/main.js
@@ -16,6 +16,28 @@ document.getElementById('add').addEventListener('click', function() {
   }
 });
 
+function removeItem() {
+  var item = this.parentNode.parentNode;
+  var parent = item.parentNode;
+
+  parent.removeChild(item);
+
+}
+
+function completeItem() {
+  var item = this.parentNode.parentNode;
+  var parent = item.parentNode;
+  var id = parent.id;
+
+  // Check if the item should be added to completed list or re-added to todo list
+  var target = (id === 'todo') ? document.getElementById('completed'):document.getElementById('todo');
+
+
+  parent.removeChild(item);
+  target.insertBefore(item, target.childNodes[0]);
+}
+
+
 //Adds a new item to the todo list
 function addItemTodo(text) {
 //  console.log(item);
@@ -30,11 +52,19 @@ function addItemTodo(text) {
 
   var remove = document.createElement('button');
   remove.classList.add('remove');
-  remove.innerHTML =removeSVG;
+  remove.innerHTML = removeSVG;
+
+  //Add click event for removing item
+  remove.addEventListener('click', removeItem);
+
 
   var complete = document.createElement('button');
   complete.classList.add('complete');
   complete.innerHTML = completeSVG;
+
+  //Add click event for completing item
+  complete.addEventListener('click', completeItem);
+
 
   buttons.appendChild(remove);
   buttons.appendChild(complete);
